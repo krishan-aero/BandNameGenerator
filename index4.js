080887db3c76d693bb8b342a852d4c86a5c309ef
Krishan <75919169+krishan-aero@users.noreply.github.com>
2024-02-17 03:16:24 +0900

    cleaning and optimized code

--- index4.js
+++ index4.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import {dirname} from "path";
 import { fileURLToPath, urlToHttpOptions } from "url";
+var bandName = "";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -10,16 +11,21 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bandNameGenerator);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
 
 app.post("/submit", (req, res) => {
-  console.log(req.body);
-  // res.write('<h1>Your band name is:</h1>');
-  res.send('<h1>Your Band name:</h1>' + req.body.street + req.body.pet);
+  res.send(`<h1>Your Band name:</h1><h2>${bandName}</h2>`);
 })
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+function bandNameGenerator (req, res, next){
+  bandName = req.body["street"] + req.body["pet"];
+  return next();
+}
